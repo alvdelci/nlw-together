094f52a541129d48f8e930a0b1b27b323fe13af5
alvdelci <delcimariosantos@gmail.com>
2021-06-22 00:32:24 -0300

    Definindo statusBar global

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
 
 import { SignIn } from './src/screens/SignIn';
-import { StatusBar } from 'react-native';
+import { Home } from './src/screens/Home';
+import { Background } from './src/components/Background';
 
 export default function App() {
 
@@ -21,13 +23,14 @@ export default function App() {
     return <AppLoading />
   }
 
-  return (<>
-    <StatusBar
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
-    />
-    <SignIn />
-  </>
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </Background>
   );
 }
