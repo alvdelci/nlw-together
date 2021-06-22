b9a776f81447b11913c42b875f4a82d62806670b
alvdelci <delcimariosantos@gmail.com>
2021-06-22 00:35:22 -0300

    Removendo statusBar e atualizando textos e estilos

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StatusBar } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
@@ -10,16 +10,10 @@ export function SignIn() {
     return (
         <View style={styles.container}>
 
-            <StatusBar 
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
-
             <Image
-            source={IllustrationImg}
-            style={styles.image}
-            resizeMode="stretch"
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
             />
 
             <View style={styles.content}>
@@ -30,13 +24,13 @@ export function SignIn() {
                 </Text>
 
                 <Text style={styles.subtitle}>
-                    Crie grupos para jogar seus games {`\n`}
+                    Crie grupos para jogar seus games {'\n'}
                     favoritos com seus amigos
                 </Text>
 
                 <ButtonIcon
-                title="Entrar com Discord"
-                activeOpacity={0.7}
+                    title="Entrar com Discord"
+                    activeOpacity={0.7}
                 />
 
             </View>
