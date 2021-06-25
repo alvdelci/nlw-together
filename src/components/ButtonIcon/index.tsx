a206ee798bb9ac7b4db029e89b8d3203366940c6
alvdelci <delcimariosantos@gmail.com>
2021-06-24 21:29:48 -0300

    Criando componente de botão

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Text, Image, View, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { Text, Image, View } from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
 import DiscordImg from '../../assets/discord.png';
 import { styles } from './styles';
 
 //Pode ser feito com interface tambem (interface {})
-type Props = TouchableOpacityProps & {
+type Props = RectButtonProps & {
     title: string;
     
 }
 
 export function ButtonIcon({ title, ...rest } : Props) {
     return (
-        <TouchableOpacity style={styles.container} {...rest} >
+        <RectButton style={styles.container} {...rest} >
             <View style={styles.iconWrapper}>
                 <Image source={DiscordImg} style={styles.icon} />
             </View>
@@ -20,6 +21,6 @@ export function ButtonIcon({ title, ...rest } : Props) {
             <Text style={styles.title}>
                 {title}
             </Text>
-        </TouchableOpacity>
+        </RectButton>
     );
 }
