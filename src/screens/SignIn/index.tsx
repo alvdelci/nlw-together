63a4593381df47c443d621ff6271dab7da7742da
alvdelci <delcimariosantos@gmail.com>
2021-06-24 21:30:19 -0300

    Adicionando navegação na tela de login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 import { ButtonIcon } from '../../components/ButtonIcon';
 
+
 export function SignIn() {
+    const navigation = useNavigation();
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
 
     return (
         <View style={styles.container}>
@@ -30,7 +37,7 @@ export function SignIn() {
 
                 <ButtonIcon
                     title="Entrar com Discord"
-                    activeOpacity={0.7}
+                    onPress={handleSignIn}
                 />
 
             </View>
