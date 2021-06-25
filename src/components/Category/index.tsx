5eaaaa89d8f6d6c37f9b4f782a69fe854cb61598
alvdelci <delcimariosantos@gmail.com>
2021-06-24 22:01:52 -0300

    Removendo checkbox

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -10,6 +10,7 @@ import { theme } from '../../global/styles/theme';
 type Props = RectButtonProps & {
     title: string;
     icon: React.FC<SvgProps>;
+    hasCheckBox?: boolean;
     checked?: boolean;
 }
 
@@ -17,10 +18,11 @@ export function Category({
     title,
     icon: Icon,
     checked = false,
+    hasCheckBox = false,
     ...rest
 }: Props) {
 
-    const { secondary50, secondary70 } = theme.colors;
+    const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
 
     return (
         <RectButton {...rest}>
@@ -29,10 +31,16 @@ export function Category({
                 colors={[secondary50, secondary70]}
             >
 
-                <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-                    <View
-                        style={checked ? styles.checked : styles.check}
-                    />
+                <LinearGradient
+                    style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+                    colors={[checked ? secondary85 : secondary50, secondary40]}
+                >
+                    {
+                        hasCheckBox &&
+                        <View
+                            style={checked ? styles.checked : styles.check}
+                        />
+                    }
                     <Icon
                         width={48}
                         height={48}
@@ -41,7 +49,7 @@ export function Category({
                     <Text style={styles.title}>
                         {title}
                     </Text>
-                </View>
+                </LinearGradient>
             </LinearGradient>
         </RectButton>
     );
