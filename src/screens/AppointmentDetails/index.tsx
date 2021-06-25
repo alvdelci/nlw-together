5ba42c3da0168d63686f561675a6f0cc62b88295
alvdelci <delcimariosantos@gmail.com>
2021-06-24 23:39:29 -0300

    Criando tela de detalhes do servidor

---
+++ src/screens/AppointmentDetails/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { ImageBackground, Text, View, FlatList } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Fontisto } from '@expo/vector-icons';
+
+import { ListDivider } from '../../components/ListDivider';
+import { ListHeader } from '../../components/ListHeader';
+import { Background } from '../../components/Background';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { Header } from '../../components/Header';
+import { Member } from '../../components/Member';
+
+import BannerImg from '../../assets/banner.png'
+
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
+
+export function AppointmentDetails() {
+    const members = [
+        {
+            id: '1',
+            username: 'Rafithy',
+            avatar_url: 'https://github.com/alvdelci.png',
+            status: 'online'
+        },
+        {
+            id: '2',
+            username: 'Rafithy',
+            avatar_url: 'https://github.com/alvdelci.png',
+            status: 'online'
+        },
+        {
+            id: '3',
+            username: 'Rafithy',
+            avatar_url: 'https://github.com/alvdelci.png',
+            status: 'offline'
+        },
+    ]
+
+    return (
+        <Background>
+            <Header
+                title="Detalhes"
+                action={
+                    <BorderlessButton>
+                        <Fontisto
+                            name="share"
+                            size={24}
+                            color={theme.colors.primary}
+                        />
+                    </BorderlessButton>
+                }
+            />
+
+            <ImageBackground
+                source={BannerImg}
+                style={styles.banner}
+            >
+                <View style={styles.bannerContent}>
+                    <Text style={styles.title}>
+                        Lendários
+                    </Text>
+                    <Text style={styles.subtitle}>
+                        É hoje que nego não dorme. Chama!
+                    </Text>
+                </View>
+            </ImageBackground>
+
+            <ListHeader
+                title="Jogadores"
+                subtitle="Total 3"
+            />
+
+            <FlatList
+                data={members}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <Member data={item} />
+                )}
+                ItemSeparatorComponent={() => <ListDivider />}
+                style={styles.members}
+            />
+
+            <View style={styles.footer}>
+                <ButtonIcon title="Entar na partida" />
+            </View>
+
+        </Background>
+    )
+}
