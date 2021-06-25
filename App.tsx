60ed1c6988243c279ccaf67b22b192092b13ed32
alvdelci <delcimariosantos@gmail.com>
2021-06-24 21:29:36 -0300

    Adicionando navegação entre telas

--- App.tsx
+++ App.tsx
@@ -5,8 +5,7 @@ import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
 
-import { SignIn } from './src/screens/SignIn';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 
 export default function App() {
@@ -24,13 +23,14 @@ export default function App() {
   }
 
   return (
+    //Envolve todos os componentes das telas com o Background
     <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      <Routes />
     </Background>
   );
 }
