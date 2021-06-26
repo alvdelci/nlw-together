a2a74b99ea389d3bc2ec8bd65fb2dfc740c246e4
alvdelci <delcimariosantos@gmail.com>
2021-06-26 19:44:02 -0300

    Finalizando tela de agendamento de eventos

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -5,17 +5,30 @@ import { RectButton } from 'react-native-gesture-handler';
 
 import { CategorySelect } from '../../components/CategorySelect';
 import { SmallInput } from '../../components/SmallInput';
-import { TextArea } from '../../components/TextArea';
-import { Background } from '../../components/Background';
+import { ModalView } from '../../components/ModalView';
 import { GuildIcon } from '../../components/GuildIcon';
+import { TextArea } from '../../components/TextArea';
+import { GuildProps } from '../../components/Guild';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
+import { Guilds } from '../Guilds';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
 
 export function AppointmentCreate() {
     const [category, setCategory] = useState('');
+    const [openGuildModal, setOpenGuildModal] = useState(false);
+    const [guild, setGuild] = useState<GuildProps>({} as GuildProps);
+
+    function handleOpenGuilds() {
+        setOpenGuildModal(true);
+    }
+
+    function handleGuildSelect(guildSelected: GuildProps) {
+        setGuild(guildSelected);
+        setOpenGuildModal(false);
+    }
 
     return (
         <KeyboardAvoidingView
@@ -41,18 +54,23 @@ export function AppointmentCreate() {
                 />
 
                 <View style={styles.form}>
-                    <RectButton>
+                    <RectButton onPress={handleOpenGuilds}>
 
                         <View style={styles.select}>
 
                             {
-                                // <View style={styles.image} />
-                                <GuildIcon />
+                                guild.icon
+                                    ? <GuildIcon />
+                                    : <View style={styles.image} />
                             }
 
                             <View style={styles.selectBody}>
                                 <Text style={styles.label}>
-                                    Selecione um servidor
+                                    {
+                                        guild.name
+                                            ? guild.name
+                                            : 'Selecione um servidor'
+                                    }
                                 </Text>
                             </View>
 
@@ -104,21 +122,22 @@ export function AppointmentCreate() {
                             Max 100 caracteres
                         </Text>
                     </View>
-
                     <TextArea
                         multiline
                         maxLength={100}
                         numberOfLines={5}
                     //autoCorrect={false}
                     />
-
                     <View style={styles.footer}>
                         <Button title="Agendar" />
                     </View>
-
                 </View>
-
             </ScrollView>
+
+            <ModalView visible={openGuildModal}>
+                <Guilds handleGuildSelect={handleGuildSelect} />
+            </ModalView>
+
         </KeyboardAvoidingView>
     )
 }
